49e4c0bb422c64260a024781182785db492690cc
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-05-04 19:54:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-2xl font-bold flex items-center justify-center border-4">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
